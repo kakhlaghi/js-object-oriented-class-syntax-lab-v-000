d941f956f77e41ca93aa3da4a53353afc0491177
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-19 20:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ class BoardMember{
   }
 
   sayHi(){
-    return `Hi my name is ${}`
+    return `Hi my name is ${this.name}. I am from ${this.homeState}, and I was trained in <training>.`
   }
 
 }
